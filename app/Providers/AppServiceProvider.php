3ad26ce47075e411d44bdc3380f0daf7e308df83
Bruno Fernando <bruno2fernando@gmail.com>
2018-01-05 09:07:47 -0300

    Add default String Length

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,8 @@ class AppServiceProvider extends ServiceProvider
         setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
         date_default_timezone_set('America/Sao_Paulo');
         Carbon::setLocale('pt_BR');
+        \Schema::defaultStringLength(191);
+
     }
 
     /**
